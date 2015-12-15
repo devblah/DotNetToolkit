235004fe3951009db829efe508765a3055326007
trialgod <trialgod@gmail.com>
2015-12-15 11:05:25 +0100

    Initial Commit

---
+++ DevBlah.DotNetToolkit/Linq/Expressions/ExpressionExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace DevBlah.DotNetToolkit.Linq.Expressions
+{
+    public static class ExpressionExtensions
+    {
+        /// <summary>
+        /// returns the property name including the object path
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public static string GetFullPropertyName<T, TProperty>(this Expression<Func<T, TProperty>> exp)
+        {
+            MemberExpression memberExp;
+            if (!TryFindMemberExpression(exp.Body, out memberExp))
+                return string.Empty;
+
+            var memberNames = new Stack<string>();
+            do
+            {
+                memberNames.Push(memberExp.Member.Name);
+            }
+            while (TryFindMemberExpression(memberExp.Expression, out memberExp));
+
+            return string.Join(".", memberNames.ToArray());
+        }
+
+        /// <summary>
+        /// gets the PropertyInfo object from an expression pointing to the associated property
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public static PropertyInfo GetPropertyInfo<T, TProperty>(this Expression<Func<T, TProperty>> exp)
+        {
+            MemberExpression memberExp;
+            if (!TryFindMemberExpression(exp.Body, out memberExp))
+            {
+                throw new ArgumentException("The expression does not point to a property.");
+            }
+
+            var propertyInfo = memberExp.Member as PropertyInfo;
+            if (propertyInfo == null)
+            {
+                throw new ArgumentException("The expression does not point to a property.");
+            }
+
+            return propertyInfo;
+        }
+
+        /// <summary>
+        /// tries to find the member expression regardless of conversions
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <param name="memberExp"></param>
+        /// <returns></returns>
+        private static bool TryFindMemberExpression(Expression exp, out MemberExpression memberExp)
+        {
+            memberExp = exp as MemberExpression;
+            if (memberExp != null)
+            {
+                return true;
+            }
+
+            // if the compiler created an automatic conversion,
+            // it'll look something like...
+            // obj => Convert(obj.Property) [e.g., int -> object]
+            // OR:
+            // obj => ConvertChecked(obj.Property) [e.g., int -> long]
+            // ...which are the cases checked in IsConversion
+            if (IsConversion(exp) && exp is UnaryExpression)
+            {
+                memberExp = ((UnaryExpression)exp).Operand as MemberExpression;
+                if (memberExp != null)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if the current expression is a convert expression
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        private static bool IsConversion(Expression exp)
+        {
+            return (
+                exp.NodeType == ExpressionType.Convert ||
+                exp.NodeType == ExpressionType.ConvertChecked
+            );
+        }
+    }
+}
---
+++ DevBlah.DotNetToolkit/Linq/IEnumerableExtensions.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevBlah.DotNetToolkit.Linq
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Traverse<T>(this IEnumerable<T> items,
+            Func<T, IEnumerable<T>> childSelector)
+        {
+            var stack = new Stack<T>(items);
+            while (stack.Any())
+            {
+                var next = stack.Pop();
+                yield return next;
+                if (childSelector(next) != null)
+                {
+                    foreach (var child in childSelector(next))
+                    {
+                        stack.Push(child);
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> TraverseOfType<TSource, TResult>(this IEnumerable<TSource> items,
+            Func<TResult, IEnumerable<TSource>> childSelector)
+        {
+            var stack = new Stack<TResult>(items.OfType<TResult>());
+            while (stack.Any())
+            {
+                var next = stack.Pop();
+                yield return next;
+                if (childSelector(next) != null)
+                {
+                    foreach (var child in childSelector(next).OfType<TResult>())
+                    {
+                        stack.Push(child);
+                    }
+                }
+            }
+        }
+    }
+}
